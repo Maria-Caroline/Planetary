0423a4dce338e2cd0b440b780c10def3624d6f50
mary <105462946+mariazinhagameplys@users.noreply.github.com>
2023-09-14 16:55:05 -0300

    Update card.js

---
+++ planetaire/src/components/card/card.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect, useTransition } from 'react';
+import './tales.css';
+import { useRef } from 'react';
+import { Link } from "react-router-dom";
+// import lilysmall from '../../assets/lily.webp'
+// import lilybigger from '../../assets/lily-bigger.webp'
+// import lilymedium from '../../assets/lily-medium.webp'
+import { useTranslation } from 'react-i18next';
+import { Trans } from 'react-i18next';
+import Header from '../../components/header/header';
+
+function Card() {
+    const { t } = useTranslation();
+
+    return (
+        <></>
+
+    );
+};
+
+export default Card;
