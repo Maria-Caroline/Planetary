4c27506f22f1e30a904b850743b981a79009b5f7
mary <105462946+mariazinhagameplys@users.noreply.github.com>
2023-09-19 20:42:01 -0300

    deck *almost* working

--- planetaire/src/pages/game/game.js
+++ planetaire/src/pages/game/game.js
@@ -3,30 +3,71 @@ import './game.css';
 import { useRef } from 'react';
 import { Link } from "react-router-dom";
 // import lilysmall from '../../assets/lily.webp'
-// import lilybigger from '../../assets/lily-bigger.webp'
-// import lilymedium from '../../assets/lily-medium.webp'
+import backcard from '../../assets/cards/temporary-back-card.png'
+import placeholder from '../../assets/cards/temporary-placeholder.png'
 import { useTranslation } from 'react-i18next';
 import { Trans } from 'react-i18next';
 import Card from "../../components/card/card"
 import Header from '../../components/header/header';
 import cardData from '../../components/card/card-data.json';
 
+
 function Game() {
-    const { t } = useTranslation();
-
-
+    const [deck, setDeck] = useState([]);
+    const [cardsDistributed, setCardsDistributed] = useState(false);
+  
+    const distributeCards = () => {
+      if (!cardsDistributed) {
+        const randomCardIds = generateRandomCardIds(); // Função para gerar IDs aleatórios
+        setDeck(randomCardIds);
+        setCardsDistributed(true);
+      }
+    };
+  
+    const generateRandomCardIds = () => {
+      const randomIds = [];
+      while (randomIds.length < 5) {
+        const randomId = Math.floor(Math.random() * cardData.planets.length) + 1;
+        if (!randomIds.includes(randomId)) {
+          randomIds.push(randomId);
+        }
+      }
+      return randomIds;
+    };
+  
+    const [revealedCardId, setRevealedCardId] = useState(null);
+  
+    const handleCardClick = (index) => {
+      // Quando uma carta virada para baixo é clicada, revelamos a carta correspondente
+      setRevealedCardId(deck[index]);
+    };
+  
     return (
-        <><Header />
-
-            <div className='container-game'>
-                
-                {/* {cardData.planets.map(planet => (
-                    <Card key={planet.id} cardData={planet} />
-                ))} */}
-            </div>
-
-        </>
+      <div>
+        <div className="game-area">
+          <button onClick={distributeCards} disabled={cardsDistributed}>
+            Distribuir Cartas
+          </button>
+          {cardsDistributed ? (
+            deck.map((cardId, index) => (
+              <div key={index} className="card" onClick={() => handleCardClick(index)}>
+                {revealedCardId === cardId ? (
+                  <Card cardData={cardData.planets[cardId - 1]} />
+                ) : (
+                  <img src={backcard} alt="Card Back" />
+                )}
+              </div>
+            ))
+          ) : (
+            Array.from({ length: 5 }, (_, index) => (
+              <div key={index} className="card">
+                <img src={backcard} alt="Card Back" />
+              </div>
+            ))
+          )}
+        </div>
+      </div>
     );
-};
-
-export default Game;
+  }
+  
+  export default Game;
