a87aaed642c4d41d605d41318be7b6af300a39c4
mary <105462946+mariazinhagameplys@users.noreply.github.com>
2023-09-28 14:14:03 -0300

    Create splashPage.js

---
+++ planetaire/src/pages/splash-page/splashPage.js
@@ -0,0 +1,13 @@
+// SplashPage.js
+import React from 'react';
+
+const SplashPage = () => {
+  return (
+    <div className="splash">
+      <img src="caminho-para-sua-logo.png" alt="Logo" />
+      <p>Seu breve resumo aqui.</p>
+    </div>
+  );
+}
+
+export default SplashPage;
